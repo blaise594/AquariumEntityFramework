da68e944b4e7e4762d6f4534a9c6a9e3b5a02674
blaise594 <danielbrogers594@gmail.com>
2017-04-17 17:01:11 -0400

    first commit

---
+++ AquariumEntityFramework/AquariumEntityFramework/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AquariumEntityFramework.DataContext;
+using AquariumEntityFramework.Models;
+
+namespace AquariumEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new AquariumsContext();
+
+            // SELECT * FROM Aquariumss
+            var AllAquariums = db.Aquariumss.ToList();
+
+            //CREATE!
+            var aquarium = new Aquariums
+            {
+                Name = "Tennessee Aquarium",
+                City = "Chatanooga"
+            };
+
+            db.Aquariumss.Add(aquarium);
+            db.SaveChanges();
+
+
+        }
+    }
+}
