049394a1ccb75a9b5661c1ecfd250a7ef6dcc75b
blaise594 <danielbrogers594@gmail.com>
2017-04-18 15:53:38 -0400

    added oceans in configuration file

--- AquariumRevised/AquariumRevised/Migrations/Configuration.cs
+++ AquariumRevised/AquariumRevised/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace AquariumRevised.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using AquariumRevised.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<AquariumRevised.DataContext.AALOContext>
     {
@@ -26,6 +28,16 @@ namespace AquariumRevised.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            var oceans = new List<Ocean>
+            {
+                new Ocean{Name = "Atlantic", AverageTemperature = 57},
+                new Ocean{Name = "Pacific", AverageTemperature = 42},
+                new Ocean{Name = "Indian", AverageTemperature = 77},
+                new Ocean{Name = "Arctic", AverageTemperature = 29}
+
+            };
+
+            oceans.ForEach(oc => context.Oceans.AddOrUpdate(o => o.Name, oc));
         }
     }
 }
